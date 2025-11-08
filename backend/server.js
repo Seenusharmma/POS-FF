45880a854df2d5b1e29e380e2669d3148254e8f0
Seenusharma <roshansharma7250@gmail.com>
2025-11-09 02:09:45 +0530

    update backend

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const server = createServer(app);
 // ✅ Setup Socket.IO with CORS
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://food-fantasy-cgu.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
